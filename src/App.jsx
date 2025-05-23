9ad871608d961a83f489033093cedf59a4615948
Kimchangwooo <changwooo2000@gmail.com>
2025-05-24 01:28:11 +0900

    대거수정7

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// src/App.jsx
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage.jsx';
 import AuthCallback from './pages/AuthCallback.jsx';
@@ -9,9 +11,13 @@ import DataImputation from './pages/DataImputation.jsx';
 export default function App() {
   return (
     <Routes>
+      {/* 로그인 관련 */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/auth/callback" element={<AuthCallback />} />
+      <Route path="/login/oauth2/success" element={<AuthCallback />} />
+      <Route path="/login/oauth2/failure" element={<LoginPage />} />
 
+      {/* 로그인 후 메인 레이아웃 + 서브 페이지 */}
       <Route path="/" element={<MainLayout />}>
         <Route index element={<ElectricityAnalysis />} />
         <Route path="usage" element={<RealTimeUsage />} />
