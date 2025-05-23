eaa826a0a403090240abb8a61a6086fad3ac7532
Kimchangwooo <changwooo2000@gmail.com>
2025-05-24 01:19:14 +0900

    대거수정5

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage            from './pages/LoginPage.jsx'
 import AuthCallback         from './pages/AuthCallback.jsx'
-import MainLayout          from './layout/MainLayout.jsx'
-import ElectricityAnalysis from './pages/ElectricityAnalysis.jsx'
-import RealTimeUsage       from './pages/RealTimeUsage.jsx'
-import DataImputation      from './pages/DataImputation.jsx'
+import MainLayout           from './layout/MainLayout.jsx'
+import ElectricityAnalysis  from './pages/ElectricityAnalysis.jsx'
+import RealTimeUsage        from './pages/RealTimeUsage.jsx'
+import DataImputation       from './pages/DataImputation.jsx'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/auth/callback" element={<AuthCallback />} />
+      {/* 로그인 관련 */}
+      <Route path="/login"           element={<LoginPage />} />
+      <Route path="/auth/callback"   element={<AuthCallback />} />
 
-      <Route path="/" element={<MainLayout />}>
-        <Route index      element={<ElectricityAnalysis />} />
-        <Route path="usage"      element={<RealTimeUsage />} />
+      {/* 보호된 루트: 로그인 후에만 진입 */}
+      <Route path="/" element={<RequireAuth><MainLayout /></RequireAuth>}>
+        <Route index         element={<ElectricityAnalysis />} />
+        <Route path="usage"  element={<RealTimeUsage />} />
         <Route path="imputation" element={<DataImputation />} />
       </Route>
+
+      {/* 위에 있는 어떤 경로에도 매칭되지 않으면 로그인으로 */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   )
 }
